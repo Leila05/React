ac7bce45aab066395f03cfa0d3fde3598a639346
Leila05 <57287485+Leila05@users.noreply.github.com>
2024-09-30 10:50:31 -0400

    Add files via upload

---
+++ reactVite-09-24/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>¡Bienvenido a mi aplicación con React!</h1>
+      <h2 className='subtitulo'>Listas de cosas por hacer:</h2>
+      <div className='contenedor'>
+        <p>Aprender React</p>
+        <p>Practicar con Vite</p>
+        <p>Construir proyectos increíbles</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
