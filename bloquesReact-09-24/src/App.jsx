65cb25238c43a2fdd7513ddefad5bd1bdd5136c8
Leila05 <57287485+Leila05@users.noreply.github.com>
2024-09-30 10:52:08 -0400

    Add files via upload

---
+++ bloquesReact-09-24/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='contenedor'>
+        <div className="top-nav"></div>
+        <div className='main'>
+          <div className='sub-content'></div>
+          <div className='sub-content'></div>
+          <div className='sub-content'></div>
+          <div className='advertisement'></div>
+        </div>
+        <div className='side-nav'></div>
+      </div>
+    </>
+  )
+}
+
+export default App;
